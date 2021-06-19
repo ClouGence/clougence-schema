908459823fa1198c10cd96a22f46a09c5acd85f7
zyc <zyc@hasor.net>
2021-06-19 16:52:37 +0800

    fix(Json): remove JsonIgnore

--- src/main/java/com/clougence/schema/umi/UniSchemaAttributes.java
+++ src/main/java/com/clougence/schema/umi/UniSchemaAttributes.java
@@ -4,7 +4,6 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import net.hasor.utils.CollectionUtils;
@@ -15,32 +14,26 @@ public class UniSchemaAttributes {
 
     private Map<String, String> attribute = new HashMap<>();
 
-    @JsonIgnore
     public Enumeration<String> keys() {
         return CollectionUtils.asEnumeration(this.attribute.keySet().iterator());
     }
 
-    @JsonIgnore
     public boolean containsKey(String attrName) {
         return this.attribute.containsKey(attrName);
     }
 
-    @JsonIgnore
     public String getValue(String attrName) {
         return this.attribute.get(attrName);
     }
 
-    @JsonIgnore
     public void setValue(String attrName, String value) {
         this.attribute.put(attrName, value);
     }
 
-    @JsonIgnore
     public void setAttributes(UniSchemaAttributes attributes) {
         this.attribute.putAll(attributes.attribute);
     }
 
-    @JsonIgnore
     public Map<String, String> toMap() {
         return this.attribute;
     }
